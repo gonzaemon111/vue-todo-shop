a7d0cdac9c6affaa7f85bd494cb9b6760bb939f2
gomzaemon111 <goki14914@icloud.com>
2020-03-29 03:12:56 +0900

    add vuetify plugin

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  devServer: {
-      port: 4000,
-      disableHostCheck: true,
-      // disableHostCheckプロパティについて、ただし、プロキシサーバーのバックエンドに開発サーバーを置いて開発するような場合、このパラメーターを入れないとWebsocketでエラーが発生します。実際に詰まった部分ですので、ついでに入れておきました。
+  "devServer": {
+    "port": 4000,
+    "disableHostCheck": true
   },
-};
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
