7248159e9ce6e0cbf78f5cf89c9d7a1d5b987310
gomzaemon111 <goki14914@icloud.com>
2020-03-30 02:32:56 +0900

    add src/main.js

--- src/main.js
+++ src/main.js
@@ -15,11 +15,16 @@ var firebaseConfig = {
   databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+
+export const db = firebase.firestore();
+// db.settings({ timestampsInSnapshots: true })
 
 new Vue({
   router,
