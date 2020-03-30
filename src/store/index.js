5a7473d5214acd6af1eb21c34b4c4946d60bd7dc
gomzaemon111 <goki14914@icloud.com>
2020-03-31 04:31:00 +0900

    add store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    message: 'Hello Vue',
+    users: [
+      { name: `hoge1`, email: `hoge1@hoge.com`, age: 20 },
+      { name: `hoge2`, email: `hoge2@hoge.com`, age: 25 },
+      { name: `hoge3`, email: `hoge3@hoge.com`, age: 30 },
+    ],
+    count: 0
+  },
+  getters: {
+    users(state) {
+      return state.users.filter(user => user.age < 30);
+    }
   },
   mutations: {
+    increment(state) {
+      state.count++;
+    },
+    decrement(state, number) {
+      state.count = state.count - number;
+    },
   },
   actions: {
   },
